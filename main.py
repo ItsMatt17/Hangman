7bcc7dc82dfda4c661fb09570f9fdc0b19beb1d6
Matthew <mattspycrafter@gmail.com>
2022-11-21 21:18:02 +0000

    Added letters guessed

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
-#Step 5
-
 import random
 from hangman_art import stages, logo
 from hangman_words import word_list
-
-
-
-#Create blanks
-
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
-
-    #Check guessed letter
-
-
-    #Check if user is wrong.
-        #TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.
-  
-
-    #Join all the elements in the list and turn it into a String.
-
   
 
 
@@ -34,17 +16,17 @@ print(logo)
 givenLetters = []
 while lives != 0 and "".join(a) != randomWord:
 	userLetter = input("Please enter a character to guess: ").lower()
+	
+	
 	if len(userLetter) > 1 or userLetter.isalpha() == False:
 		print('invalid input')
 	else:
 		isFound = 0
-	
 		for i in range(0, len(randomWord)):
 			if userLetter == randomWord[i]:
 				a.pop(i)
 				isFound += 1
 				a.insert(i, userLetter)
-				#print(isFound)
 		
 		
 		if isFound == 0 and  userLetter not in givenLetters:
@@ -63,9 +45,9 @@ while lives != 0 and "".join(a) != randomWord:
 				
 		
 		
-			
+		print(f"Letters guessed: {givenLetters}")
 		print("".join(a))
-
+	
 		
 if "".join(a) == randomWord:
 	print(f"You won the word was {randomWord}")
